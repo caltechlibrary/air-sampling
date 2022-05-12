401046632df3b9dcc5d237a8c9c3f3402b7e6b85
Kian Badie <kianbadie@gmail.com>
2022-05-12 09:04:55 -0700

    Rewrite header to have no internal functions since they are not necessary.

--- scripts/header.js
+++ scripts/header.js
@@ -1,17 +1,12 @@
 let Header = function(root) {
     
     let dateEl = root.querySelector(".header__date");
-
-    let init = function() {
-        let date = new Date();
-        let minutesFormatted = (date.getMinutes() >= 10) ? date.getMinutes(): `0${date.getMinutes()}`;
-        let period = (date.getHours() >= 12) ? "pm" : "am";
-        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        let timeString = `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()} ${date.getHours() % 12}:${minutesFormatted} ${period}`;
-        let dateTextNode = document.createTextNode(timeString);
-        dateEl.appendChild(dateTextNode);
-    };
-
-    init();
+    let date = new Date();
+    let minutesFormatted = (date.getMinutes() >= 10) ? date.getMinutes(): `0${date.getMinutes()}`;
+    let period = (date.getHours() >= 12) ? "pm" : "am";
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let timeString = `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()} ${date.getHours() % 12}:${minutesFormatted} ${period}`;
+    let dateTextNode = document.createTextNode(timeString);
+    dateEl.appendChild(dateTextNode);
 
 };
