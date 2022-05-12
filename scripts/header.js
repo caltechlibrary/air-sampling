d6ded39c6bbeb0a914108ea3f80a98ca2d148cb1
Kian Badie <kianbadie@gmail.com>
2022-05-12 09:27:07 -0700

    Rewrite header date to use built in date functions.

--- scripts/header.js
+++ scripts/header.js
@@ -2,11 +2,9 @@ let Header = function(root) {
     
     let dateEl = root.querySelector(".header__date");
     let date = new Date();
-    let minutesFormatted = (date.getMinutes() >= 10) ? date.getMinutes(): `0${date.getMinutes()}`;
-    let period = (date.getHours() >= 12) ? "pm" : "am";
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let timeString = `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()} ${date.getHours() % 12}:${minutesFormatted} ${period}`;
-    let dateTextNode = document.createTextNode(timeString);
+    let time = date.toLocaleTimeString("en-US", { hour12: true, timeStyle: "short" });
+    let day = date.toLocaleDateString("en-US", { dateStyle: "medium" })
+    let dateTextNode = document.createTextNode(`${day} ${time}`);
     dateEl.appendChild(dateTextNode);
 
 };
