5bbfb4b7b61c52f8e67667f48a46bc69e23ec472
Thomas Morrell <tmorrell@users.noreply.github.com>
2022-05-17 16:09:38 -0700

    Add error condition for missing data

--- get-air-values/lambda_function.py
+++ get-air-values/lambda_function.py
@@ -11,26 +11,36 @@ table = dynamodb.Table('air-sampling-table')
 def lambda_handler(event, context):
     today = datetime.date.today().isoformat()
     scan = table.query(KeyConditionExpression=Key('date').eq(today),Limit=1,ConsistentRead=True,ScanIndexForward=False)
-    data = scan['Items'][0]
-    export = {}
-    for value in data:
-        if value != 'date':
-            export[value] = float(data[value])
-        else:
-            export[value] = data[value]
-    myaqi = aqi.to_aqi([
-        (aqi.POLLUTANT_PM25,export['PM2.5']),
-        (aqi.POLLUTANT_PM10,export['PM10']),
-        (aqi.POLLUTANT_NO2_1H,export['NO2']),
-        (aqi.POLLUTANT_SO2_1H,export['SO2'])
-        ])
-    export['aqi'] = float(myaqi)
-    return {
-        'statusCode': 200,
-        'headers': {
-            "Access-Control-Allow-Headers" : "Content-Type",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "OPTIONS,GET"
-        },
-        'body': json.dumps(export)
-    }
+    try:
+        data = scan['Items'][0]
+        export = {}
+        for value in data:
+            if value != 'date':
+                export[value] = float(data[value])
+            else:
+                export[value] = data[value]
+        myaqi = aqi.to_aqi([
+            (aqi.POLLUTANT_PM25,export['PM2.5']),
+            (aqi.POLLUTANT_PM10,export['PM10']),
+            (aqi.POLLUTANT_NO2_1H,export['NO2']),
+            (aqi.POLLUTANT_SO2_1H,export['SO2'])
+            ])
+        export['aqi'] = float(myaqi)
+        return {
+            'statusCode': 200,
+            'headers': {
+                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,GET"
+            },
+            'body': json.dumps(export)
+        }
+    except IndexError:
+        return {
+            'statusCode': 503,
+            'headers': {
+                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,GET"
+            }
+       }
