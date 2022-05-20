a4ff561c785fb0e6e37f5f9e599217dde3799113
Kian Badie <kianbadie@gmail.com>
2022-05-20 11:44:26 -0700

    Add functionality to display aqi widget appropraitely when there is no data.

--- scripts/AqiWidget.js
+++ scripts/AqiWidget.js
@@ -5,8 +5,14 @@ let AqiWidget = function(root, value) {
     let inidicatorEl = root.querySelector(".aqi-widget__meter-indicator");
     let maxAqiValue = parseInt(meterEl.getAttribute("aria-valuemax"));
 
-    valueEl.textContent = value;
-    meterEl.setAttribute("aria-valuenow", value);
-    inidicatorEl.style.left = `${(value / maxAqiValue) * 100}%`;
+    if(value) {
+        valueEl.textContent = value;
+        meterEl.setAttribute("aria-valuenow", value);
+        inidicatorEl.style.left = `${(value / maxAqiValue) * 100}%`;
+    } else {
+        root.classList.add("aqi-widget--data-unavailble");
+        valueEl.textContent = "Not available";
+        meterEl.setAttribute("aria-valuetext", "Not available");
+    }
 
 };
