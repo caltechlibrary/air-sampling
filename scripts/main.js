b1d9d6d0a26d741100c7312bbafeb829e0fc792e
Kian Badie <kianbadie@gmail.com>
2022-07-24 15:11:04 -0700

    Add missing semicolon to initializeHeader function.

--- scripts/main.js
+++ scripts/main.js
@@ -29,7 +29,7 @@
         let dateEl = document.querySelector(".header__date");
         let date = new Date();
         let time = date.toLocaleTimeString("en-US", { hour12: true, timeStyle: "short" });
-        let day = date.toLocaleDateString("en-US", { dateStyle: "medium" })
+        let day = date.toLocaleDateString("en-US", { dateStyle: "medium" });
         let dateTextNode = document.createTextNode(`${day} ${time}`);
         dateEl.appendChild(dateTextNode);
     };
