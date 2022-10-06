0426e9dfb055ee715d5044cf91772000096c9116
Kian Badie <kianbadie@gmail.com>
2022-10-05 21:17:21 -0700

    Replace text node in header intialization with text content.

--- scripts/main.js
+++ scripts/main.js
@@ -35,8 +35,7 @@
         let date = new Date();
         let time = date.toLocaleTimeString("en-US", { hour12: true, timeStyle: "short" });
         let day = date.toLocaleDateString("en-US", { dateStyle: "medium" });
-        let dateTextNode = document.createTextNode(`${day} ${time}`);
-        dateEl.appendChild(dateTextNode);
+        dateEl.textContent = `${day} ${time}`;
     };
 
     let fetchCurrentValuesAndMappings = async function() {
