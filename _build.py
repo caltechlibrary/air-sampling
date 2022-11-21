f3c06a8624eacb512d44ce8395e1e6e3ca9781f2
Kian Badie <kianbadie@gmail.com>
2022-11-21 12:57:18 -0800

    Add PyYAML package and use it to create pollutants.json mapping.

--- _build.py
+++ _build.py
@@ -1,11 +1,14 @@
 import os
 import shutil
 import subprocess
+import yaml
+import json
 
 outDir = "site"
 stylesDir = "styles"
 scriptsDir = "scripts"
 imagesDir = "img"
+pollutantsDir = "pollutants"
 
 # Create output site directory
 if os.path.isdir(outDir): shutil.rmtree(outDir)
@@ -17,6 +20,22 @@ shutil.copyfile("air-values.json", f"{outDir}/air-values.json")
 shutil.copyfile("air-data.txt", f"{outDir}/air-data.txt")
 shutil.copyfile("mappings.json", f"{outDir}/mappings.json")
 shutil.copyfile("google9c66b3b3d14f628e.html", f"{outDir}/google9c66b3b3d14f628e.html")
+
+# Build pollutant mappings
+with open(f"{outDir}/pollutants.json", "w", encoding="utf-8") as mappingsFile: 
+    mappingsDict = {}
+
+    for pollutantDir in os.scandir(pollutantsDir):
+        conditions = []
+
+        for condition in os.scandir(pollutantDir):
+            with open(condition, "r", encoding="utf-8") as conditionFile:
+                conditions.append(yaml.safe_load(conditionFile))
+                
+        conditions.sort(key=lambda condition: condition["range"])
+        mappingsDict[pollutantDir.name] = conditions
+
+    json.dump(mappingsDict, mappingsFile)
 
 # Create index page
 subprocess.run(["pandoc", "--from=markdown", "--to=html", f"--output={outDir}/index.html", "--template=templates/index.html", "index.md"])
