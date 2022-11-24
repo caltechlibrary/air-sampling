3bb3c4f69706a1a591edbcda067fe32b17c7e85a
Kian Badie <kianbadie@gmail.com>
2022-11-24 14:10:20 -0800

    Convert pollutant files/mappings to only describe warning messages.

--- _build.py
+++ _build.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import subprocess
+import pathlib
 import yaml
 import json
 
@@ -21,19 +22,13 @@ shutil.copyfile("air-data.txt", f"{outDir}/air-data.txt")
 shutil.copyfile("google9c66b3b3d14f628e.html", f"{outDir}/google9c66b3b3d14f628e.html")
 
 # Build pollutant mappings
+mappingsDict = {}
+for pollutantFilename in os.scandir(pollutantsDir):
+        pollutantName = pathlib.Path(pollutantFilename.name).stem
+        with open(pollutantFilename, "r", encoding="utf-8") as pollutantFile:          
+            mappingsDict[pollutantName] = yaml.safe_load(pollutantFile)
+            
 with open(f"{outDir}/mappings.json", "w", encoding="utf-8") as mappingsFile: 
-    mappingsDict = {}
-
-    for pollutantDir in os.scandir(pollutantsDir):
-        conditions = []
-
-        for condition in os.scandir(pollutantDir):
-            with open(condition, "r", encoding="utf-8") as conditionFile:
-                conditions.append(yaml.safe_load(conditionFile))
-                
-        conditions.sort(key=lambda condition: condition["range"])
-        mappingsDict[pollutantDir.name] = conditions
-
     json.dump(mappingsDict, mappingsFile)
 
 # Create index page
