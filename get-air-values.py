d975f6b966bb1c7c12da6099aae7bd6b5306b972
Thomas Morrell <tmorrell@users.noreply.github.com>
2022-12-22 11:20:25 -0800

    Graphs in progress

--- get-air-values.py
+++ get-air-values.py
@@ -1,7 +1,6 @@
 import boto3
 import json
 import datetime
-import aqi
 from boto3.dynamodb.conditions import Key
 
 print('Loading function')
@@ -19,13 +18,6 @@ def lambda_handler(event, context):
                 export[value] = float(data[value])
             else:
                 export[value] = data[value]
-        myaqi = aqi.to_aqi([
-            (aqi.POLLUTANT_PM25,export['PM2.5']),
-            (aqi.POLLUTANT_PM10,export['PM10']),
-            (aqi.POLLUTANT_NO2_1H,export['NO2']),
-            (aqi.POLLUTANT_SO2_1H,export['SO2'])
-            ])
-        export['aqi'] = float(myaqi)
         return {
             'statusCode': 200,
             'headers': {
