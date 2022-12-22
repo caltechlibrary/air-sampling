7681c08052efcdb99a51364b3d2b15abf0e75f02
Thomas Morrell <tmorrell@users.noreply.github.com>
2022-12-22 14:15:32 -0800

    Final graphing version

--- get-air-values.py
+++ get-air-values.py
@@ -3,36 +3,68 @@ import json
 import datetime
 from boto3.dynamodb.conditions import Key
 
-print('Loading function')
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('air-sampling-table')
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("air-sampling-table")
+
 
 def lambda_handler(event, context):
     today = datetime.date.today().isoformat()
-    scan = table.query(KeyConditionExpression=Key('date').eq(today),Limit=1,ConsistentRead=True,ScanIndexForward=False)
+    today = "2022-08-01"
+    scan = table.query(
+        KeyConditionExpression=Key("date").eq(today),
+        ConsistentRead=True,
+        ScanIndexForward=False,
+    )
     try:
-        data = scan['Items'][0]
-        export = {}
-        for value in data:
-            if value != 'date':
-                export[value] = float(data[value])
-            else:
-                export[value] = data[value]
-        return {
-            'statusCode': 200,
-            'headers': {
-                "Access-Control-Allow-Headers" : "Content-Type",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "OPTIONS,GET"
-            },
-            'body': json.dumps(export)
-        }
+        if event["queryStringParameters"]:
+            metric = event["queryStringParameters"]["graph"]
+            data = ""
+            for line in scan["Items"]:
+                if metric not in line:
+                    return {
+                        "statusCode": 503,
+                        "headers": {
+                            "Access-Control-Allow-Headers": "Content-Type",
+                            "Access-Control-Allow-Origin": "*",
+                            "Access-Control-Allow-Methods": "OPTIONS,GET",
+                        },
+                    }
+                time = line["time"]
+                time = datetime.datetime.fromtimestamp(time).time()
+                data = data + f"{time},{line[metric]}\n"
+            return {
+                "headers": {"Content-Type": "text/csv"},
+                "statusCode": 200,
+                "headers": {
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "OPTIONS,GET",
+                },
+                "body": data,
+            }
+        else:
+            data = scan["Items"][0]
+            export = {}
+            for value in data:
+                if value != "date":
+                    export[value] = float(data[value])
+                else:
+                    export[value] = data[value]
+            return {
+                "statusCode": 200,
+                "headers": {
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "OPTIONS,GET",
+                },
+                "body": json.dumps(export),
+            }
     except IndexError:
         return {
-            'statusCode': 503,
-            'headers': {
-                "Access-Control-Allow-Headers" : "Content-Type",
+            "statusCode": 503,
+            "headers": {
+                "Access-Control-Allow-Headers": "Content-Type",
                 "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "OPTIONS,GET"
-            }
-       }
+                "Access-Control-Allow-Methods": "OPTIONS,GET",
+            },
+        }
