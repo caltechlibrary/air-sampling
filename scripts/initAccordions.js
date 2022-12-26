fe92a5aa206e2eb654e0d7c856d77b88111ec257
Kian Badie <kianbadie@gmail.com>
2022-12-26 13:21:40 -0800

    Modify accordions script to use regular function and loop and dedicated onToggle event listener.

--- scripts/initAccordions.js
+++ scripts/initAccordions.js
@@ -1,15 +1,19 @@
+function onToggle(e) {
+    const toggleBtn = e.currentTarget;
+    const pollutantWidgetEl = toggleBtn.closest(".pollutant-widget");
+
+    if(toggleBtn.getAttribute("aria-expanded") == "false") {
+        pollutantWidgetEl.classList.add("pollutant-widget--expanded");
+        toggleBtn.setAttribute("aria-expanded", "true");
+    } else {
+        pollutantWidgetEl.classList.remove("pollutant-widget--expanded");
+        toggleBtn.setAttribute("aria-expanded", "false");
+    }
+}
+
 const pollutantWidgetEls = document.querySelectorAll(".pollutant-widget");
 
-pollutantWidgetEls.forEach(pollutantWidgetEl => {
+for(const pollutantWidgetEl of pollutantWidgetEls) {
     const toggleBtn = pollutantWidgetEl.querySelector(".pollutant-widget__toggle-btn");
-
-    toggleBtn.addEventListener("click", function(){
-        if(toggleBtn.getAttribute("aria-expanded") == "false") {
-            pollutantWidgetEl.classList.add("pollutant-widget--expanded");
-            toggleBtn.setAttribute("aria-expanded", "true");
-        } else {
-            pollutantWidgetEl.classList.remove("pollutant-widget--expanded");
-            toggleBtn.setAttribute("aria-expanded", "false");
-        }
-    });
-})
+    toggleBtn.addEventListener("click", onToggle);
+}
