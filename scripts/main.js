637fb2456d2216c5312be8a187c772703c3560d8
Kian Badie <kianbadie@gmail.com>
2022-12-26 12:19:16 -0800

    Add functionality to support new graph endpoint for pollutant data.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import { fetchJSON, fetchCSV } from "./modules/fetchHelpers.js";
 import getConditionFromAQIMapping from "./modules/getConditionFromAQIMapping.js";
 import getPollutantDataFromCSV from "./modules/getPollutantDataFromCSV.js";
+import timeElementToDate from "./modules/timeElementToDate.js";
 import pollutantChart from "./modules/pollutantChart.js";
 
 let onResize = function() {
@@ -126,14 +127,17 @@ try{
 
 for(let pollutantWidgetEl of pollutantWidgetEls) initializePollutantWidgetAccordion(pollutantWidgetEl.getAttribute("data-pollutant"));
 
-let pollutantCSVString = await fetchCSV("air-data.txt");
-let pollutantData = getPollutantDataFromCSV(pollutantCSVString);
-
 for(let pollutantWidgetEl of pollutantWidgetEls) {
-    let pollutant = pollutantWidgetEl.getAttribute("data-pollutant");
-    let chartContainer = pollutantWidgetEl.querySelector(".pollutant-widget__chart-container");
-    let {data, unit} = pollutantData[pollutant];
-    let chartSvg = pollutantChart(data, { pollutant, unit });
+    const pollutant = pollutantWidgetEl.getAttribute("data-pollutant");
+    const unit = pollutantWidgetEl.getAttribute("data-unit");
+    const chartContainer = pollutantWidgetEl.querySelector(".pollutant-widget__chart-container");
+
+    const pollutantDataCSVString = await fetchCSV(`https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=${pollutant}`);
+    const pollutantData = getPollutantDataFromCSV(pollutantDataCSVString);
+    const pollutantDataFormatted = pollutantData.map(datum => ({ ...datum, time: timeElementToDate(datum.time) }));
+
+    const chartSvg = pollutantChart(pollutantDataFormatted, { pollutant, unit });
     chartSvg.classList.add("pollutant-widget__chart");
+
     chartContainer.append(chartSvg);
 }
--- scripts/modules/getPollutantDataFromCSV.js
+++ scripts/modules/getPollutantDataFromCSV.js
@@ -1,25 +1,10 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.7.0/+esm";
 
 function getPollutantDataFromCSV(csvString) {
-    let pollutantData = {};
-    let rows = d3.csvParse(csvString, d3.autoType);
-    let timeField = rows.columns[0];
+    const pollutantData = [];
+    const rows = d3.csvParseRows(csvString, d3.autoType);
 
-    for(let pollutant of rows.columns) {
-        if(pollutant != timeField) {
-            pollutantData[pollutant] = { unit: rows[0][pollutant], data: [] };
-        }
-    }
-
-    for(let i = 1; i < rows.length; i++) {
-        let row = rows[i];
-        let time = row[timeField] * 1000;
-        for(let pollutant in row) {
-            if(pollutant != timeField) {
-                pollutantData[pollutant].data.push({ time, value: row[pollutant] });
-            }
-        }
-    }
+    rows.forEach(([time, value]) => pollutantData.push({ time, value }));
 
     return pollutantData;
 };
