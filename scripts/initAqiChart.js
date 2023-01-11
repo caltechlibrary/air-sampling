f17b03e849435351123091b4f50d4dbb8f2b73ef
Kian Badie <kianbadie@gmail.com>
2023-01-10 19:22:34 -0800

    Replace dummy data file with generated dummy data in initAqiChart.

--- scripts/initAqiChart.js
+++ scripts/initAqiChart.js
@@ -3,8 +3,6 @@ import getPollutantDataFromCSV from "./modules/getPollutantDataFromCSV.js";
 import timeElementToDate from "./modules/timeElementToDate.js";
 import aqiChart from "./modules/aqiChart.js";
 
-import { aqiDataLower, aqiDataUpper, tempDataLower, tempDataUpper } from "./modules/dummyAQIData.js";
-
 const csvData = await Promise.all([
     fetchCSV(`https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=aqi`),
     fetchCSV(`https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=temp`)
@@ -13,6 +11,13 @@ const csvData = await Promise.all([
 const [aqiData, tempData] = csvData
     .map(csvString => getPollutantDataFromCSV(csvString))
     .map(csvData => csvData.map(csvEntry => ({ ...csvEntry, time: timeElementToDate(csvEntry.time) })));
+
+// Dummy data
+const aqiDataLower = aqiData.map(entry => ({ ...entry, value: entry.value - 1.5 }));
+const aqiDataUpper = aqiData.map(entry => ({ ...entry, value: entry.value + 1.5 }));
+const tempDataLower = tempData.map(entry => ({ ...entry, value: entry.value - 1.5 }));
+const tempDataUpper = tempData.map(entry => ({ ...entry, value: entry.value + 1.5 }));
+/////////////
 
 const chartSVG = aqiChart(aqiData, aqiDataLower, aqiDataUpper, tempData, tempDataLower, tempDataUpper);
 const chartContainer = document.querySelector(".aqi-chart");
