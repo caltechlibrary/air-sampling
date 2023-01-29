a2970682cca2fd6644328db0e51316e5a78f1cb1
Kian Badie <kianbadie@gmail.com>
2023-01-29 13:02:11 -0800

    Add dummy area graph data script and use it in aqi chart.

---
+++ generate-dummy-area-data.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+import csv
+
+def generateDummyAreaData(file, value):
+    with open(file, 'w', newline='') as f:
+        writer = csv.writer(f)
+        for hour in range(24):
+            for min in range(60):
+                writer.writerow([f'{hour:02}:{min:02}:00', value])
+
+if os.path.isdir('dummy-area-data'): shutil.rmtree('dummy-area-data')
+os.makedirs('dummy-area-data')
+generateDummyAreaData('dummy-area-data/aqi_upper.csv', 35)
+generateDummyAreaData('dummy-area-data/aqi_lower.csv', 30)
+generateDummyAreaData('dummy-area-data/temp_upper.csv', 15)
+generateDummyAreaData('dummy-area-data/temp_lower.csv', 10)
--- scripts/initAqiChart.js
+++ scripts/initAqiChart.js
@@ -5,19 +5,16 @@ import aqiChart from "./modules/aqiChart.js";
 
 const csvData = await Promise.all([
     fetchCSV(`https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=aqi`),
-    fetchCSV(`https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=temp`)
+    fetchCSV(`https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=temp`),
+    fetchCSV(`dummy-area-data/aqi_lower.csv`),
+    fetchCSV(`dummy-area-data/aqi_upper.csv`),
+    fetchCSV(`dummy-area-data/temp_lower.csv`),
+    fetchCSV(`dummy-area-data/temp_upper.csv`)
 ]);
 
-const [aqiData, tempData] = csvData
+const [aqiData, tempData, aqiDataUpper, aqiDataLower, tempDataLower, tempDataUpper] = csvData
     .map(csvString => getPollutantDataFromCSV(csvString))
     .map(csvData => csvData.map(csvEntry => ({ ...csvEntry, time: timeElementToDate(csvEntry.time) })));
-
-// Dummy data
-const aqiDataLower = aqiData.map(entry => ({ ...entry, value: entry.value - 1.5 }));
-const aqiDataUpper = aqiData.map(entry => ({ ...entry, value: entry.value + 1.5 }));
-const tempDataLower = tempData.map(entry => ({ ...entry, value: entry.value - 1.5 }));
-const tempDataUpper = tempData.map(entry => ({ ...entry, value: entry.value + 1.5 }));
-/////////////
 
 const chartSVG = aqiChart(aqiData, aqiDataLower, aqiDataUpper, tempData, tempDataLower, tempDataUpper);
 const chartContainer = document.querySelector(".aqi-chart");
