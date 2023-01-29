3c1b38f578d2d1b467900b857e51ae9ed19d9c0d
Kian Badie <kianbadie@gmail.com>
2023-01-29 13:21:44 -0800

    Refactor initAqiChart to increase readability

--- scripts/initAqiChart.js
+++ scripts/initAqiChart.js
@@ -3,18 +3,22 @@ import getPollutantDataFromCSV from "./modules/getPollutantDataFromCSV.js";
 import timeElementToDate from "./modules/timeElementToDate.js";
 import aqiChart from "./modules/aqiChart.js";
 
-const csvData = await Promise.all([
+const res = await Promise.all([
     fetchCSV(`https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=aqi`),
-    fetchCSV(`https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=temp`),
     fetchCSV(`dummy-area-data/aqi_lower.csv`),
     fetchCSV(`dummy-area-data/aqi_upper.csv`),
+    fetchCSV(`https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=temp`),
     fetchCSV(`dummy-area-data/temp_lower.csv`),
     fetchCSV(`dummy-area-data/temp_upper.csv`)
 ]);
 
-const [aqiData, tempData, aqiDataUpper, aqiDataLower, tempDataLower, tempDataUpper] = csvData
-    .map(csvString => getPollutantDataFromCSV(csvString))
-    .map(csvData => csvData.map(csvEntry => ({ ...csvEntry, time: timeElementToDate(csvEntry.time) })));
+const csvData = res.map(getPollutantDataFromCSV);
+
+const csvDataTimeFormatted = csvData.map(data => {
+    return data.map(entry => ({ ...entry, time: timeElementToDate(entry.time) }))
+});
+
+const [aqiData, aqiDataLower, aqiDataUpper, tempData, tempDataLower, tempDataUpper] = csvDataTimeFormatted;
 
 const chartSVG = aqiChart(aqiData, aqiDataLower, aqiDataUpper, tempData, tempDataLower, tempDataUpper);
 const chartContainer = document.querySelector(".aqi-chart");
