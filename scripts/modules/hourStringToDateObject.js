c429e733eac7967074682aba75c915f3ba33affd
Kian Badie <kianbadie@gmail.com>
2023-02-05 15:47:46 -0800

    Add documentation to hourStringToDateObject

--- scripts/modules/hourStringToDateObject.js
+++ scripts/modules/hourStringToDateObject.js
@@ -1,5 +1,12 @@
-function hourStringToDateObject(timeElement) {
-    const [hh, mm, ss] = timeElement.split(":");
+/**
+ * Converts hour string to JS Date object. Assumes hour string is 
+ * the time at the current day locale time.
+ * 
+ * @param {string} hourString Hour string in HH:MM:SS format
+ * @returns {Date} Date object
+ */
+function hourStringToDateObject(hourString) {
+    const [hh, mm, ss] = hourString.split(":");
     const date = new Date();
 
     date.setHours(hh, mm, ss);
