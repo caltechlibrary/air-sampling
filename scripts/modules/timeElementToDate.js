4938473a6742214dd7a032ed21027eaa5c93de18
Kian Badie <kianbadie@gmail.com>
2023-02-05 15:39:37 -0800

    Refactor timeElementToDate

--- scripts/modules/timeElementToDate.js
+++ scripts/modules/timeElementToDate.js
@@ -1,8 +1,9 @@
 function timeElementToDate(timeElement) {
-    const date = new Date();
     const [hh, mm, ss] = timeElement.split(":");
-    date.setHours(hh, mm, ss, 0);
-    return new Date(date);
+    const date = new Date();
+    
+    date.setHours(hh, mm, ss);
+    return date;
 }
 
 export default timeElementToDate;
