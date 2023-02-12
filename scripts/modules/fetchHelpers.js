788b1e0657ddc158c5ef140e3725bfe31aaffe6f
Kian Badie <kianbadie@gmail.com>
2023-02-12 14:34:29 -0800

    Change exports of fetchHelpers

--- scripts/modules/fetchHelpers.js
+++ scripts/modules/fetchHelpers.js
@@ -1,15 +1,10 @@
-async function fetchJSON(resource) {
+export async function fetchJSON(resource) {
     const res = await fetch(resource);
     if(!res.ok) throw new Error("Network response was not OK");
     return await res.json();
 }
 
-async function fetchCSV(resource) {
+export async function fetchCSV(resource) {
     const res = await fetch(resource);
     return await res.text();
 }
-
-export { 
-    fetchJSON, 
-    fetchCSV 
-};
