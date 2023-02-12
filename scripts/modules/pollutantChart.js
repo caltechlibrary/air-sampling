c8f0986473ab6db098cbc2a19dbcd2e01dc0fee0
Kian Badie <kianbadie@gmail.com>
2023-02-12 10:52:40 -0800

    Remove unecessary () in pollutant chart custom time format

--- scripts/modules/pollutantChart.js
+++ scripts/modules/pollutantChart.js
@@ -28,7 +28,7 @@ function pollutantChart(data, {
         const xRange = [marginLeft, width - marginRight];
         const yRange = [height - marginBottom, marginTop];
 
-        const customTimeFormat = (date) => date.toLocaleString("en-US", { timeZone: "America/Los_Angeles", timeStyle: "short" })
+        const customTimeFormat = date => date.toLocaleString("en-US", { timeZone: "America/Los_Angeles", timeStyle: "short" })
 
         // Construct scales and axes.
         const xScale = d3.scaleTime(xDomain, xRange);
