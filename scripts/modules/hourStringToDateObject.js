1aae7b3a82bbcf694e9b501ee1bfc4da504cca0e
Kian Badie <kianbadie@gmail.com>
2023-02-12 14:35:30 -0800

    Delete documentation for hourStringToDateObject

--- scripts/modules/hourStringToDateObject.js
+++ scripts/modules/hourStringToDateObject.js
@@ -1,12 +1,5 @@
 import * as luxon from "https://cdn.jsdelivr.net/npm/luxon@3.2.1/+esm"
 
-/**
- * Converts hour string to JS Date object. Assumes hour string is 
- * the time at the current day locale time.
- * 
- * @param {string} hourString Hour string in HH:MM:SS format
- * @returns {Date} Date object
- */
 function hourStringToDateObject(hourString) {
     const [hh, mm, ss] = hourString.split(":");
     const dateLuxon = luxon.DateTime.fromObject(
