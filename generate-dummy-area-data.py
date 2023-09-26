7994f89cfe6b0caad9eff331de9e4bdf3104a2ff
KianBadie <kianbadie@gmail.com>
2023-09-26 08:17:54 -0700

    Modify dummy area data script to not delete parent dir

--- generate-dummy-area-data.py
+++ generate-dummy-area-data.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import csv
 
 dummyDir = "dummy"
@@ -11,9 +10,15 @@ def generateDummyAreaData(file, value):
             for min in range(60):
                 writer.writerow([f"{hour:02}:{min:02}:00", value])
 
-if os.path.isdir(dummyDir): shutil.rmtree(dummyDir)
-os.makedirs(dummyDir)
-generateDummyAreaData(f"{dummyDir}/aqi_upper.csv", 45)
-generateDummyAreaData(f"{dummyDir}/aqi_lower.csv", 40)
-generateDummyAreaData(f"{dummyDir}/temp_upper.csv", 15)
-generateDummyAreaData(f"{dummyDir}/temp_lower.csv", 10)
+def generateDummyAreaFile(file, value):
+    try:
+        os.remove(file)
+    except OSError:
+        pass
+
+    generateDummyAreaData(file, value)
+
+generateDummyAreaFile(f"{dummyDir}/aqi_upper.csv", 45)
+generateDummyAreaFile(f"{dummyDir}/aqi_lower.csv", 40)
+generateDummyAreaFile(f"{dummyDir}/temp_upper.csv", 15)
+generateDummyAreaFile(f"{dummyDir}/temp_lower.csv", 10)
