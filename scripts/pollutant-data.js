5f654adcdd0fdbc8f8bf5320d97cdd892bb6fb03
KianBadie <kianbadie@gmail.com>
2023-11-11 14:50:00 -0800

    Update pollutant data table to include bands

--- scripts/pollutant-data.js
+++ scripts/pollutant-data.js
@@ -1,20 +1,35 @@
-import { fetchCSV } from "./modules/fetchHelpers.js";
+import { fetchCSV, fetchJSON } from "./modules/fetchHelpers.js";
 import parseCsv from "./modules/parseCsv.js";
+import parseBands from "./modules/parseBands.js";
+import aggreageData from "./modules/aggregateData.js";
 import hourStringToDateObject from "./modules/hourStringToDateObject.js";
 import createDataTable from "./modules/createDataTable.js";
 
 const DATA_LABELS = {
     time: "Time",
-    value: "Value"
+    pollutantData: "Value",
+    pollutantDataLower: "Lower",
+    pollutantDataUpper: "Upper",
 };
 
 const formula = document.documentElement.getAttribute("data-formula");
 const main = document.querySelector("main");
 
-const pollutantDataCSVString = await fetchCSV(`https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=${formula}`);
-const pollutantData = parseCsv(pollutantDataCSVString);
-const pollutantDataFormatted = pollutantData.map(datum => ({ ...datum, time: hourStringToDateObject(datum.time) }));
+const pollutantCsv = await fetchCSV(`https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=${formula}`);
+const bandsData = await fetchJSON("https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=bands");
 
-const table = createDataTable(pollutantDataFormatted, DATA_LABELS);
+const pollutantData = parseCsv(pollutantCsv).map(datum => ({ ...datum, time: hourStringToDateObject(datum.time) }));
+const pollutantBandsData = parseBands(bandsData[`time_${formula.replace(".", "")}`]).map(entry => ({ ...entry, time: new Date(entry.time), }));
+const pollutantDataLower = pollutantBandsData.map(entry => ({ time: entry.time, value: entry.lower }));
+const pollutantDataUpper = pollutantBandsData.map(entry => ({ time: entry.time, value: entry.upper }));
+
+const dataAggregated = aggreageData({
+    pollutantData,
+    pollutantDataLower,
+    pollutantDataUpper
+});
+
+
+const table = createDataTable(dataAggregated, DATA_LABELS);
 
 main.appendChild(table);
