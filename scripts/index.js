120018e15d7a9e67181f1662ad1f67d303fdf785
KianBadie <kianbadie@gmail.com>
2023-11-14 20:42:00 -0800

    Move chart init to index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,9 @@
 import fetchJSON from "./modules/fetchJSON.js";
+import fetchCSV from "./modules/fetchCSV.js";
+import parseCsv from "./modules/parseCsv.js";
+import hourStringToDateObject from "./modules/hourStringToDateObject.js";
+import parseBands from "./modules/parseBands.js";
+import { aqiChart, pollutantChart } from "./modules/charts.js";
 
 function getCondition(aqi) {
     let condition;
@@ -80,4 +85,97 @@ async function initCurrentValues() {
     }
 }
 
+async function initAqiChart(bandsData) {
+    const [aqiCsv, tempCsv] = await Promise.all([
+        fetchCSV("https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=aqi"),
+        fetchCSV("https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=temp"),
+    ]);
+
+    const aqiData = parseCsv(aqiCsv).map(row => ({ ...row, time: hourStringToDateObject(row.time) }));
+    const tempData = parseCsv(tempCsv).map(row => ({ ...row, time: hourStringToDateObject(row.time) }));
+    const aqiBandsData = parseBands(bandsData.time_AQI);
+    const tempBandsData = parseBands(bandsData.time_T);
+
+    const chartContainer = document.querySelector(".aqi-chart__chart-container");
+
+    function generateAqiChart() {
+        const prevChartSvg = document.querySelector(".aqi-chart__chart-svg");
+        const chartHeight = window.innerWidth > 600 ? 400 : 300;
+        const chartWidth = chartContainer.offsetWidth;
+    
+        const chartSVG = aqiChart(aqiData, aqiBandsData, tempData, tempBandsData, {
+            height: chartHeight,
+            width: chartWidth
+        });
+    
+        chartSVG.classList.add("aqi-chart__chart-svg");
+    
+        prevChartSvg?.remove();
+        chartContainer.append(chartSVG);
+    }
+
+    generateAqiChart();
+    window.addEventListener('resize', generateAqiChart);
+}
+
+async function initPollutantChart(bandsData, pollutantWidgetEl) {
+    const pollutantWidgetToggleEl = pollutantWidgetEl.querySelector(".pollutant-widget__toggle-btn");
+    const chartContainer = pollutantWidgetEl.querySelector(".pollutant-widget__chart");
+
+    const pollutant = pollutantWidgetEl.getAttribute("data-pollutant");
+    const unit = pollutantWidgetEl.getAttribute("data-unit");
+
+    const pollutantCsv = await fetchCSV(`https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=${pollutant}`);
+
+    const pollutantData = parseCsv(pollutantCsv).map(row => ({ ...row, time: hourStringToDateObject(row.time) }));
+    const pollutantBandsData = parseBands(bandsData[`time_${pollutant.replace(".", "")}`]);
+
+    function generatePollutantChart() {
+        const chartWidth = chartContainer.offsetWidth;
+        const chartHeight = chartContainer.offsetHeight;
+    
+        const chartSVG = pollutantChart(pollutantData, pollutantBandsData, {
+            height: chartHeight,
+            width: chartWidth,
+            pollutant,
+            unit
+        });
+    
+        chartSVG.classList.add("pollutant-widget__chart-svg")
+    
+        chartContainer.replaceChildren(chartSVG);
+    }
+
+    pollutantWidgetToggleEl.addEventListener("click", () => {
+        if(window.innerWidth < 800 && pollutantWidgetEl.classList.contains("pollutant-widget--expanded")) {
+            generatePollutantChart();
+        }
+    });
+
+    pollutantWidgetEl.addEventListener("transitionend", (event) => {
+        if(event.propertyName == "height" && pollutantWidgetEl.classList.contains("pollutant-widget--expanded")) {
+            generatePollutantChart();
+        }
+    });
+
+    window.addEventListener("resize", () => {
+        if(pollutantWidgetEl.classList.contains("pollutant-widget--expanded")) {
+            generatePollutantChart();
+        }
+    });
+}
+
+async function initCharts() {
+    const bandsData = await fetchJSON("https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=bands");
+
+    initAqiChart(bandsData);
+
+    const pollutantWidgetEls = document.querySelectorAll(".pollutant-widget");
+
+    for(const pollutantWidgetEl of pollutantWidgetEls) {
+        initPollutantChart(bandsData, pollutantWidgetEl);
+    }
+}
+
 initCurrentValues();
+initCharts();
