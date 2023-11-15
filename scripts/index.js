64622482685ac3621fb36dfba37be7c425b60020
KianBadie <kianbadie@gmail.com>
2023-11-14 20:51:34 -0800

    Move accordion logic to index.js

--- scripts/index.js
+++ scripts/index.js
@@ -201,5 +201,21 @@ async function initCharts() {
 onResize();
 window.addEventListener("resize", onResize);
 
+const pollutantWidgetEls = document.querySelectorAll(".pollutant-widget");
+
+for(const pollutantWidgetEl of pollutantWidgetEls) {
+    const pollutantWidgetToggleEl = pollutantWidgetEl.querySelector(".pollutant-widget__toggle-btn");
+
+    pollutantWidgetToggleEl.addEventListener("click", function() {    
+        if(pollutantWidgetToggleEl.getAttribute("aria-expanded") == "false") {
+            pollutantWidgetEl.classList.add("pollutant-widget--expanded");
+            pollutantWidgetToggleEl.setAttribute("aria-expanded", "true");
+        } else {
+            pollutantWidgetEl.classList.remove("pollutant-widget--expanded");
+            pollutantWidgetToggleEl.setAttribute("aria-expanded", "false");
+        }
+    });
+}
+
 initCurrentValues();
 initCharts();
