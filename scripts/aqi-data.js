680a3dcfa4abc687faa8767e9dbe4d4c148195d6
KianBadie <kianbadie@gmail.com>
2023-11-19 10:53:56 -0800

    Remove dummy functionality from aqi-data

--- scripts/aqi-data.js
+++ scripts/aqi-data.js
@@ -16,26 +16,14 @@ const DATA_LABELS = {
 }
 
 const main = document.querySelector("main");
-const dummy = document.documentElement.hasAttribute("data-dummy")
 
 let res;
 
-if (dummy) {
-    res = await Promise.all([
-        fetchCSV("dummy/aqi.csv"),
-        fetchCSV("dummy/aqi_lower.csv"),
-        fetchCSV("dummy/aqi_upper.csv"),
-        fetchCSV("dummy/temp.csv"),
-        fetchCSV("dummy/temp_lower.csv"),
-        fetchCSV("dummy/temp_upper.csv")
-    ]);
-} else {
-    res = await Promise.all([
-        fetchCSV("https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=aqi"),
-        fetchCSV("https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=temp"),
-        fetchJSON("https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=bands")
-    ]);
-}
+res = await Promise.all([
+    fetchCSV("https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=aqi"),
+    fetchCSV("https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=temp"),
+    fetchJSON("https://z44g6g2rrl.execute-api.us-west-2.amazonaws.com/test/get_air?graph=bands")
+]);
 
 const [aqiCsv, tempCsv, bandsData] = res;
 
