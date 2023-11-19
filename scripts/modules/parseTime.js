18ca9a11b3cc803892fad321266a5430b0dc8555
KianBadie <kianbadie@gmail.com>
2023-11-19 10:10:02 -0800

    Modify parseTime to set ms to 0

--- scripts/modules/parseTime.js
+++ scripts/modules/parseTime.js
@@ -1,7 +1,7 @@
 function parseTime(time) {
     const [hh, mm, ss] = time.split(":");
     const parsedTime = new Date();
-    parsedTime.setHours(parseInt(hh), parseInt(mm), parseInt(ss));
+    parsedTime.setHours(parseInt(hh), parseInt(mm), parseInt(ss), 0);
 
     return parsedTime;
 }
